a36337c3ffab31334cffc0f9df5e5ee8dfb5a14b
Spencer Maley <swmaley02@gmail.com>
2023-10-15 18:53:06 -0500

    Added requirements.txt

--- main.py
+++ main.py
@@ -14,3 +14,4 @@
 #(5/5 points) There should be a minimum of 5 commits on your project, be sure to commit often!
 #(5/5 points) I will be checking out the master branch of your project. Please be sure to include a requirements.txt file which contains all the packages that need installed. You can create this fille with the output of pip freeze at the terminal prompt.
 #(10/10 points) There should be a README.md file in your project that explains what your project is, how to install the pip requirements, and how to execute the program. Please use the GitHub flavor of Markdown. Be thorough on the explanations. You will need to explain the steps of initializing the database and then how to run the development server for your project.
+
